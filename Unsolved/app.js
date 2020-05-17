cbf37dd0032355a0fddbec87699c721585245c4b
William Trout <troutw5593@gmail.com>
2020-05-17 12:39:06 -0500

    ammended the ass class functions.

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -5,7 +5,8 @@ const Employee = require("./lib/Employee.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+// let hasManager = false;
+let employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,10 +14,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer").render;
 
 const questions = [{
-        type: "input",
-        name: "managerName",
-        message: "What manager does the employee report to?",
-    }, {
         type: "input",
         name: "name",
         message: "What is the employee's name?",
@@ -35,53 +32,71 @@ const questions = [{
         type: "list",
         name: "role",
         message: "What is the employee's role?",
-        choices: ['Manager', 'Engineer', 'Intern']
+        choices: ["Manager", "Engineer", "Intern"],
     },
     {
         type: "input",
         name: "officeNumber",
         message: "What is the Managers office number?",
         when: (answers) => {
-            return answers.role === "Manager"
-        }
+            return answers.role === "Manager";
+        },
     },
     {
         type: "input",
         name: "github",
         message: "What is the Engineer's github user name?",
         when: (answers) => {
-            return answers.role === "Engineer"
-        }
+            return answers.role === "Engineer";
+        },
     },
     {
         type: "input",
         name: "school",
         message: "What school does the Intern attend?",
         when: (answers) => {
-            return answers.role === "Intern"
-        }
+            return answers.role === "Intern";
+        },
     },
-]
+];
 
 const init = async () => {
-    let employees = []
-    const answer = await inquirer.prompt(questions)
+    const answer = await inquirer.prompt(questions);
     // Figure out what R is
     switch (answer.role) {
         case "Manager":
-            employees.push(new Manager(answer.name, answer.id, answer.email, answer.officeNumber))
+            employees.push(
+                new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
+            );
+            // init();
+            break;
+        case "Intern":
+            employees.push(
+                new Intern(answer.name, answer.id, answer.email, answer.school)
+            );
+            // init();
+            break;
+        case "Engineer":
+            employees.push(
+                new Engineer(answer.name, answer.id, answer.email, answer.github)
+            );
+            // init();
+            // hasManager = true;
+            // condition check 
+            //   ask questions to add first employee
             break;
         default:
-            console.log("Not set up yet")
+            // ask if you want to do it again. here
+            console.log("Not set up yet");
             break;
     }
     // Push he new object to emloyees
     // Figure ou if the user would like to coninue
     // Render or continue
     fs.writeFile("team.html", render(employees), () => {
-        console.log("teamMemberAdded")
-    })
-}
+        // console.log("teamMemberAdded");
+    });
+};
 // .then((inquirerResponse) => {
 //     const markdown = generateMarkdown(inquirerResponse)
 
@@ -92,7 +107,6 @@ const init = async () => {
 // });
 
 init();
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
